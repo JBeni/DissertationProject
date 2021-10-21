d29069b4992c79c16c1be1e9c6b31cd65a05a086
JBeni <JBeni@users.noreply.github.com>
2021-10-21 14:47:50 +0300

    add ts file fro sidebar and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    title = 'app';
+    menuOpened: boolean = false;
+
+    @ViewChild("header_toggle", { read: ElementRef }) toggle!: ElementRef;
+    @ViewChild("nav_bar", { read: ElementRef }) nav!: ElementRef;
+    @ViewChild("body_pd", { read: ElementRef }) bodypd!: ElementRef;
+    @ViewChild("header", { read: ElementRef }) headerpd!: ElementRef;
+
+    // @ViewChild("nav__link") navLinks!: ElementRef;
+
+    constructor() {}
+
+    menuSlide(): void {
+        if (this.menuOpened === false) {
+            this.nav.nativeElement.classList.add('show');
+            this.toggle.nativeElement.classList.add('bx-x');
+            this.bodypd.nativeElement.classList.add('body-pd');
+            this.headerpd.nativeElement.classList.add('body-pd');
+            this.menuOpened = true;
+        } else {
+            this.nav.nativeElement.classList.remove('show');
+            this.toggle.nativeElement.classList.remove('bx-x');
+            this.bodypd.nativeElement.classList.remove('body-pd');
+            this.headerpd.nativeElement.classList.remove('body-pd');
+            this.menuOpened = false;
+        }
+    }
+
+    // activeMenuOption(): void {
+    //     if(this.navLinks) {
+
+    //         this.navLinks.forEach(l => l.classList.remove('active'));
+    //         this.navLinks.nativeElement.classList.add('active');
+    //     }
+    // }
+
 }
