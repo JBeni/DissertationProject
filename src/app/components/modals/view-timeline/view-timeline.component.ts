69488024e0cced2d1cd6a92e3faba846ddee6417
JBeni <JBeni@users.noreply.github.com>
2021-10-21 22:13:34 +0300

    creating the view timeline component

---
+++ src/app/components/modals/view-timeline/view-timeline.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-view-timeline',
+    templateUrl: './view-timeline.component.html',
+    styleUrls: ['./view-timeline.component.css'],
+})
+export class ViewTimelineComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
