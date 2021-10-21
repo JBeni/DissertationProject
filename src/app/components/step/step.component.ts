f092dcdb8516e238da912233b331cde6b05016dd
JBeni <JBeni@users.noreply.github.com>
2021-10-21 22:14:19 +0300

    creating the stepper component

---
+++ src/app/components/step/step.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-step',
+    templateUrl: './step.component.html',
+    styleUrls: ['./step.component.css'],
+})
+export class StepComponent implements OnInit {
+    firstFormGroup!: FormGroup;
+    secondFormGroup!: FormGroup;
+
+    constructor(private _formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.firstFormGroup = this._formBuilder.group({
+            firstCtrl: ['', Validators.required],
+        });
+        this.secondFormGroup = this._formBuilder.group({
+            secondCtrl: ['', Validators.required],
+        });
+    }
+}
