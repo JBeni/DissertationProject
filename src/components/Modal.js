cfdd447328067ef9756858b82c323b5159c69846
JBeni <JBeni@users.noreply.github.com>
2021-11-05 23:19:16 +0200

    modal - not helpful - mostly as orientation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,21 +4,17 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { AddUser } from './Admin/AddUser';
 
 const style = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 400,
-	bgcolor: 'background.paper',
-	border: '2px solid #000',
-	boxShadow: 24,
-	p: 4,
+    position: 'absolute',
+    top: '10%',
+    left: '20%',
+	width: 800,
+    p: 4,
 };
 
-export default function TransitionsModal() {
+export default function ModalComponent() {
 	const [open, setOpen] = React.useState(false);
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
@@ -39,12 +35,7 @@ export default function TransitionsModal() {
 			>
 				<Fade in={open}>
 					<Box sx={style}>
-						<Typography id="transition-modal-title" variant="h6" component="h2">
-							Text in a modal
-						</Typography>
-						<Typography id="transition-modal-description" sx={{ mt: 2 }}>
-							Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-						</Typography>
+                        <AddUser />
 					</Box>
 				</Fade>
 			</Modal>
