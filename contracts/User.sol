f59f95aaee217e33eac5cd2030df0e8ec7451259
JBeni <JBeni@users.noreply.github.com>
2021-11-06 23:38:41 +0200

    making suitable to the app flow adding index as key store property

--- contracts/User.sol
+++ contracts/User.sol
@@ -2,10 +2,13 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract User {
-    mapping(address => S_UserData) public userInfo;
+    mapping(uint256 => S_UserData) public userInfo;
     address public owner;
+    uint256 public usersCount = 0;
+    uint256[] public userIds;
 
     struct S_UserData {
+        uint256 index;
         bytes32 username;
         bytes32 email;
         bytes32 firstname;
@@ -21,7 +24,14 @@ contract User {
         ProjectSupervisor
     }
 
-    event UserRegistered(address indexed _walletAddress, bytes32 _username, bytes32 _email, bytes32 _firstname, bytes32 _lastname, Roles _role);
+    event UserRegistered(
+        address indexed _walletAddress,
+        bytes32 _username,
+        bytes32 _email,
+        bytes32 _firstname,
+        bytes32 _lastname,
+        Roles _role
+    );
 
     constructor() {
         owner = msg.sender;
@@ -33,33 +43,52 @@ contract User {
     }
 
     modifier checkUser(address _address) {
-        require(_address == msg.sender, "Address");
+        //require(_address == msg.sender, "Address");
         _;
     }
 
     function registerUser(
-        bytes32 _username, bytes32 _email, bytes32 _firstname,
-        bytes32 _lastname, uint _role, address _walletAddress
+        bytes32 _username,
+        bytes32 _email,
+        bytes32 _firstname,
+        bytes32 _lastname,
+        uint256 _role,
+        address _walletAddress
     ) public onlyOwner {
-        userInfo[_walletAddress].username = _username;
-        userInfo[_walletAddress].email = _email;
-        userInfo[_walletAddress].firstname = _firstname;
-        userInfo[_walletAddress].lastname = _lastname;
-        userInfo[_walletAddress].role = Roles(_role);
-        userInfo[_walletAddress].walletAddress = _walletAddress;
-        emit UserRegistered(_walletAddress, _username, _email, _firstname, _lastname, Roles(_role));
+        userInfo[usersCount].index = usersCount;
+        userInfo[usersCount].username = _username;
+        userInfo[usersCount].username = _username;
+        userInfo[usersCount].email = _email;
+        userInfo[usersCount].firstname = _firstname;
+        userInfo[usersCount].lastname = _lastname;
+        userInfo[usersCount].role = Roles(_role);
+        userInfo[usersCount].walletAddress = _walletAddress;
+        emit UserRegistered(
+            _walletAddress,
+            _username,
+            _email,
+            _firstname,
+            _lastname,
+            Roles(_role)
+        );
+        userIds.push(usersCount);
+        usersCount++;
     }
 
-    function changeUserRole(uint _role, address _walletAddress) public onlyOwner returns(bytes32) {
-        userInfo[_walletAddress].role = Roles(_role);
-        return "Role Updated!";
+    // function changeUserRole(uint _role, address _walletAddress) public onlyOwner returns(string memory) {
+    //     userInfo[_walletAddress].role = Roles(_role);
+    //     return "Role Updated!";
+    // }
+
+    function getUserIds() external view returns (uint256[] memory) {
+        return userIds;
     }
 
-    function getUserInfo(address _walletAddress) external view onlyOwner returns(S_UserData memory) {
-        return userInfo[_walletAddress];
-    }
-
-    function getUser() public view onlyOwner returns(bytes32) {
-        return "Beniamin";
+    function getUserInfo(uint256 _index)
+        external
+        view
+        returns (S_UserData memory)
+    {
+        return userInfo[_index];
     }
 }
