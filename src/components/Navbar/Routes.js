d6e6370a77f980c94f6b7e2ee2f029e4ea5a0e24
JBeni <JBeni@users.noreply.github.com>
2021-11-07 12:25:25 +0200

    creating routes for every component and set the connection for blochain interaction

--- src/components/Navbar/Routes.js
+++ src/components/Navbar/Routes.js
@@ -5,17 +5,50 @@ import Projects from '../Projects';
 import Users from '../Users';
 import Dashboard from '../Dashboard';
 
-function Routes() {
-
-    return (
-        <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/users" component={Users} />
-            <Route path="/projects" component={Projects} />
-            <Route component={DefaultPage} />
-        </Switch>
-    );
+function Routes(props) {
+	return (
+		<Switch>
+			<Route
+				exact path="/"
+				render={() => (
+					<Dashboard
+						account={props.account}
+						project={props.project}
+						web3={props.web3}
+					/>
+				)}
+			/>
+			<Route
+				path="/users"
+				render={() => (
+					<Users
+						account={props.account}
+						project={props.project}
+						web3={props.web3}
+					/>
+				)}
+			/>
+			<Route
+				path="/projects"
+				render={() => (
+					<Projects
+						account={props.account}
+						project={props.project}
+						web3={props.web3}
+					/>
+				)}
+			/>
+			<Route
+				render={() => (
+					<DefaultPage
+						account={props.account}
+						project={props.project}
+						web3={props.web3}
+					/>
+				)}
+			/>
+		</Switch>
+	);
 }
 
 export default Routes;
