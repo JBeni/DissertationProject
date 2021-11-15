cd012f6110d86b6f529524279f3b5fe2573d84f1
JBeni <JBeni@users.noreply.github.com>
2021-11-15 10:54:11 +0200

    loader change color

--- src/components/Views/Loader.js
+++ src/components/Views/Loader.js
@@ -14,7 +14,7 @@ export default function Loader() {
 
 	return (
 		<div className={classes.root}>
-			<CircularProgress size={0.8 * 500} color="secondary" className="center-container" />
+			<CircularProgress size={0.8 * 500} color="primary" className="center-container" />
             <div className="text-loader">
                 <p>The Project contract not deployed to detected network.</p>
                 <p>You need to start the Ganache deployment network to run this project.</p>
