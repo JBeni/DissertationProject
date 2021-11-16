94b28adcf524802ca8b41ae79344416b18e4ca5a
JBeni <JBeni@users.noreply.github.com>
2021-11-16 22:06:01 +0200

    changes

--- src/components/Projects/AddProjectModal.js
+++ src/components/Projects/AddProjectModal.js
@@ -1,281 +1,214 @@
-import React from 'react';
-import '../Styles/AddProjectModal.css';
-import Dialog from '@mui/material/Dialog';
-import Button from '@mui/material/Button';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-const axios = require('axios');
+import React, { useState, useEffect } from 'react';
+import { Grid, TextField } from '@material-ui/core';
+import { projectStatusDropdown } from './DefaultModal';
+import { makeStyles } from '@material-ui/core/styles';
+import { useForm, Form }  from './useForm';
+import {
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	FormHelperText,
+	Button,
+} from '@material-ui/core';
 
-export const projectStatusDropdown = [
-	{ id: '1', value: 'Created', label: 'Created' },
-	{ id: '2', value: 'Approved', label: 'Approved' },
-	{ id: '3', value: 'Rejected', label: 'Rejected' },
-	{ id: '4', value: 'OnGoing', label: 'OnGoing' },
-	{
-		id: '5',
-		value: 'BeforeFinalizationCheck',
-		label: 'BeforeFinalizationCheck',
+const initialFValues = {
+	id: 0,
+	fullName: '',
+	email: '',
+	mobile: '',
+	city: '',
+	gender: 'male',
+	departmentId: '',
+	hireDate: new Date(),
+	isPermanent: false,
+};
+
+const useStyles = makeStyles((theme) => ({
+	secondary: {
+		backgroundColor: theme.palette.secondary.light,
+		'& .MuiButton-label': {
+			color: theme.palette.secondary.main,
+		},
 	},
-	{ id: '6', value: 'Completed', label: 'Completed' },
-];
+	primary: {
+		backgroundColor: theme.palette.primary.light,
+		'& .MuiButton-label': {
+			color: theme.palette.primary.main,
+		},
+	},
+	root: {
+		'& .MuiFormControl-root': {
+			width: '80%',
+			margin: theme.spacing(1),
+		},
+		minWidth: 0,
+		margin: theme.spacing(0.5),
+	},
+	pageContent: {
+		margin: '10px',
+		padding: '10px',
+	},
+	searchInput: {
+		width: '75%',
+	},
+	newButton: {
+		position: 'absolute',
+		right: '10px',
+	},
+	label: {
+		textTransform: 'none',
+	},
+	dialogWrapper: {
+		padding: theme.spacing(2),
+		position: 'absolute',
+		top: theme.spacing(5),
+	},
+	dialogTitle: {
+		paddingRight: '0px',
+	},
+}));
 
-const _hashStringValue = process.env.REACT_APP_HASH_STRING_VALUE;
+export default function AddProjectModal(props) {
+	const classes = useStyles();
+	const {addOrEdit, recordForEdit} = props;
 
-export default class AddProjectModal extends React.Component {
-	constructor(props) {
-		super(props);
+	const [values, setValues] = useState(initialFValues);
+	const [errors, setErrors] = useState({});
+	const [validateOnChange, setValidateOnChange] = useState(true);
 
-		this.state = {
-			formValues: {
-				Name: '',
-				Description: '',
-				Status: '',
-				File: '',
-			},
-			formErrors: {
-				Name: '',
-				Description: '',
-				Status: '',
-				File: '',
-			},
-			formValidity: {
-				Name: false,
-				Description: false,
-				Status: false,
-				File: true,
-			},
-			isSubmitting: false,
-			open: false,
-		};
-	}
-
-    handleClickOpen = () => {
-		this.setState({
-			formValues: {
-				Name: '',
-				Description: '',
-				Status: '',
-                File: '',
-			},
-			formErrors: {
-				Name: '',
-				Description: '',
-				Status: '',
-                File: '',
-			},
-			formValidity: {
-				Name: false,
-				Description: false,
-				Status: false,
-                File: true,
-			},
-            file_cid_pinata: '',
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value,
 		});
-		this.setState({ open: true });
-	}
-
-	fileChangeHandler = (event) => {
-		const { formValues } = this.state;
-		formValues['File'] = event.target.files[0];
-		this.setState({ formValues });
-        this.fileValidationHandler(event);
-    }
-
-    fileValidationHandler = (event) => {
-        const fieldValidationErrors = this.state.formErrors;
-		const validity = this.state.formValidity;
-		validity['File'] = event.target.files[0].name.length > 0;
-		fieldValidationErrors['File'] = validity['File']
-			? ''
-			: `This field is required.`;
-
-        this.setState({
-            formErrors: fieldValidationErrors,
-            formValidity: validity,
-        });
-    }
-
-	handleClose = (event, reason) => {
-		if (reason === 'backdropClick') {
-			return false;
-		}
-		this.setState({ open: false });
+		if (validateOnChange) validate({ [name]: value });
 	};
 
-	handleChange = ({ target }) => {
-		const { formValues } = this.state;
-		formValues[target.name] = target.value;
-		this.setState({ formValues });
-		this.handleValidation(target);
+	const resetForm = () => {
+		setValues(initialFValues);
+		setErrors({});
 	};
 
-	handleValidation = (target) => {
-		const { name, value } = target;
-		const fieldValidationErrors = this.state.formErrors;
-		const validity = this.state.formValidity;
-
-		// const isName = name === 'Name';
-		// const isDescription = name === 'Description';
-		// const isStatus = name === 'Status';
-
-		validity[name] = value.length > 0;
-		fieldValidationErrors[name] = validity[name]
-			? ''
-			: `This field is required.`;
-
-		this.setState({
-			formErrors: fieldValidationErrors,
-			formValidity: validity,
+	const validate = (fieldValues = values) => {
+		let temp = { ...errors };
+		if ('fullName' in fieldValues)
+			temp.fullName = fieldValues.fullName ? '' : 'This field is required.';
+		if ('email' in fieldValues)
+			temp.email = /$^|.+@.+..+/.test(fieldValues.email)
+				? ''
+				: 'Email is not valid.';
+		if ('mobile' in fieldValues)
+			temp.mobile =
+				fieldValues.mobile.length > 9 ? '' : 'Minimum 10 numbers required.';
+		if ('departmentId' in fieldValues)
+			temp.departmentId =
+				fieldValues.departmentId.length !== 0 ? '' : 'This field is required.';
+		setErrors({
+			...temp,
 		});
+
+		if (fieldValues === values)
+			return Object.values(temp).every((x) => x === '');
 	};
 
-    fileHandleSubmission = () => {
-        const { formValues } = this.state;
-
-        if (formValues['File'].name.length > 0) {
-            const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
-            let data = new FormData();
-            data.append('file',  formValues['File'], formValues['File'].name);
-            const metadata = JSON.stringify({
-                name: formValues['File'].name,
-                type: formValues['File'].type,
-                sizeBytes: formValues['File'].size,
-                lastModifiedDate: formValues['File'].lastModifiedDate
-            });
-            data.append('pinataMetadata', metadata);
-
-            return axios.post(url, data, {
-                maxBodyLength: 'Infinity',
-                headers: {
-                    'Content-Type': `multipart/form-data`,
-                    pinata_api_key: process.env.REACT_APP_PINATA_API_KEY,
-                    pinata_secret_api_key: process.env.REACT_APP_PINATA_API_SECRET
-                }
-            }).then(function (response) {
-                return response.data.IpfsHash;
-            }).catch(function (error) {
-                console.log(error);
-            });
-        }
-    }
-
-	handleSubmit = async (event) => {
-        this.setState({ isSubmitting: true });
-		const { formValues, formValidity } = this.state;
-		if (Object.values(formValidity).every(Boolean)) {
-			// alert('Form is validated! Submitting the form...');
-			this.setState({ isSubmitting: false });
-            let ipfsFileCID = await Promise.resolve(this.fileHandleSubmission());
-
-            await this.createProject(
-				formValues['Name'],
-				formValues['Description'],
-				formValues['Status'],
-                ipfsFileCID
-			);
-		} else {
-			for (let key in formValues) {
-				let target = {
-					name: key,
-					value: formValues[key],
-				};
-				this.handleValidation(target);
-			}
-			this.setState({ isSubmitting: false });
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (validate()) {
+			addOrEdit(values, resetForm);
 		}
 	};
 
-	createProject = async (_name, _description, _status, _ipfsFileCID) => {
-		await this.props.project.methods
-			.createProject(_name, _description, Number(_status), _hashStringValue, _ipfsFileCID)
-			.send({ from: this.props.account });
-	};
+	useEffect(() => {
+		if (recordForEdit != null)
+			setValues({
+				...recordForEdit,
+			});
+	}, [recordForEdit]);
 
-	render() {
-		const { formValues, formErrors, isSubmitting, open } =
-			this.state;
+	return (
+		<Form onSubmit={handleSubmit}>
+			<Grid container>
+				<Grid item xs={6}>
+					<TextField
+						name="fullName"
+						label="Full Name"
+						value={values.fullName}
+						onChange={handleInputChange}
+						error={errors.fullName}
+					/>
+					<TextField
+						label="Email"
+						name="email"
+						value={values.email}
+						onChange={handleInputChange}
+						error={errors.email}
+					/>
+					<TextField
+						label="Mobile"
+						name="mobile"
+						value={values.mobile}
+						onChange={handleInputChange}
+						error={errors.mobile}
+					/>
+					<TextField
+						variant="outlined"
+						label="City"
+						name="city"
+						value={values.city}
+						onChange={handleInputChange}
+					/>
+					<TextField
+						variant="outlined"
+						placeholder="MultiLine with rows: 2 and rowsMax: 4"
+						multiline
+						rows={2}
+						rowsMax={4}
+					/>
+				</Grid>
 
-		return (
-			<div>
-				<Button variant="outlined" onClick={this.handleClickOpen}>
-					Add New Project
-				</Button>
+				<Grid item xs={6}>
+					<FormControl {...(errors && { error: true })}>
+						<InputLabel>Department</InputLabel>
+						<Select
+							label="Department"
+							name="departmentId"
+							value={values.departmentId}
+							onChange={handleInputChange}
+						>
+							<MenuItem value="">None</MenuItem>
+							{projectStatusDropdown.map((item) => (
+								<MenuItem key={item.id} value={item.id}>
+									{item.title}
+								</MenuItem>
+							))}
+						</Select>
+						{errors && <FormHelperText>{errors.departmentId}</FormHelperText>}
+					</FormControl>
 
-				<Dialog open={open} onClose={this.handleClose}>
-					<div className="wrapper">
-						<div className="title">Registration Form</div>
-						<DialogContent>
-							<div className="form">
-								<div className="inputfield">
-									<label>Name</label>
-									<input
-										type="text"
-										className="input"
-										name="Name"
-										placeholder="Project Name"
-										onChange={this.handleChange}
-										value={formValues.Name}
-										// required
-									/>
-								</div>
-								<div className="error">{formErrors.Name}</div>
-
-								<div className="inputfield">
-									<label>Last Name</label>
-									<textarea
-										className="textarea"
-										name="Description"
-										placeholder="Project Description"
-										onChange={this.handleChange}
-										value={formValues.Description}
-										// required
-									></textarea>
-								</div>
-								<div className="error">{formErrors.Description}</div>
-
-								<div className="inputfield">
-									<label>Status</label>
-									<div className="custom_select">
-										<select
-											name="Status"
-											onChange={this.handleChange}
-											value={formValues.Status}
-										>
-											<option key={0} value="">
-												Select
-											</option>
-											{projectStatusDropdown.map((item) => (
-												<option key={item.id} value={item.id}>
-													{item.value}
-												</option>
-											))}
-										</select>
-									</div>
-								</div>
-								<div className="error">{formErrors.Status}</div>
-
-								<div className="inputfield">
-									<label>File Upload</label>
-									<input
-										type="file"
-										className="file"
-										name="File"
-										onChange={this.fileChangeHandler}
-									/>
-								</div>
-								<div className="error">{formErrors.File}</div>
-
-							</div>
-						</DialogContent>
-						<DialogActions>
-							<Button onClick={this.handleClose}>Close</Button>
-							<Button type="submit" onClick={this.handleSubmit}>
-								{isSubmitting ? 'Please wait...' : 'Submit'}
-							</Button>
-						</DialogActions>
+					<div>
+						<Button
+							classes={{ root: classes.root, label: classes.label }}
+							variant="contained"
+							size="large"
+							color="primary"
+							type="submit"
+						>
+							Submit
+						</Button>
+						<Button
+							classes={{ root: classes.root, label: classes.label }}
+							variant="contained"
+							size="large"
+							color="default"
+							onClick={resetForm}
+						>
+							Reset
+						</Button>
 					</div>
-				</Dialog>
-			</div>
-		);
-	}
+				</Grid>
+			</Grid>
+		</Form>
+	);
 }
