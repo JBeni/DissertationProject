81c9b99d178216a7555ab3bc4947adb033291f12
JBeni <JBeni@users.noreply.github.com>
2021-11-16 22:05:44 +0200

    company

--- src/components/CompanyBuilder/CompanyBuilder.js
+++ src/components/CompanyBuilder/CompanyBuilder.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import MaterialTable from '@material-table/core';
+
+const tableIcons = {
+};
 
 const CompanyBuilder = () => {
 	const [users, setUser] = useState([
@@ -40,51 +43,25 @@ const CompanyBuilder = () => {
 		loadUsers();
 	};
 
+    const tableRef = React.createRef();
+    const columns = [
+        { title: 'Username', field: 'username' },
+        //{ title: 'First Name', field: 'firstname' },
+        //{ title: 'Last Name', field: 'lastname' },
+        { title: 'Email', field: 'email' },
+        { title: 'Role', field: 'role' },
+        //{ title: 'Wallet Address', field: 'walletAddress' },
+    ];
+
 	return (
-		<div className="container">
-			<div className="py-4">
-				<h1>Home Page</h1>
-				<table className="table border shadow">
-					<thead className="thead-dark">
-						<tr>
-							<th scope="col">#</th>
-							<th scope="col">Name</th>
-							<th scope="col">User Name</th>
-							<th scope="col">Email</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
-						{users.map((user, index) => (
-							<tr>
-								<th scope="row">{index + 1}</th>
-								<td>{user.name}</td>
-								<td>{user.username}</td>
-								<td>{user.email}</td>
-								<td>
-									<Link className="btn btn-primary mr-2" to={`/users/${user.id}`}>
-										View
-									</Link>
-									<Link
-										className="btn btn-outline-primary mr-2"
-										to={`/users/edit/${user.id}`}
-									>
-										Edit
-									</Link>
-									<Link
-										className="btn btn-danger"
-										onClick={() => deleteUser(user.id)}
-									>
-										Delete
-									</Link>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+        <MaterialTable
+        title="Users ProjectChain"
+        tableRef={tableRef}
+        icons={tableIcons}
+        columns={columns}
+        actions={[]}
+    />
+);
 };
 
 export default CompanyBuilder;
