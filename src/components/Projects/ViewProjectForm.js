90fe8f2645bd47857d1670f61b51d6cc0a5e3198
JBeni <JBeni@users.noreply.github.com>
2021-11-17 20:21:26 +0200

    view project form

---
+++ src/components/Projects/ViewProjectForm.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, FormLabel } from '@material-ui/core';
+import { initialProjectFormValues, projectStatusDropdown } from '../applicationService';
+
+export default function ViewProjectForm(props) {
+	const { recordForEdit } = props;
+	const [values, setValues] = useState(initialProjectFormValues);
+
+	useEffect(() => {
+		if (recordForEdit != null) {
+			let status = projectStatusDropdown.find((element) => {
+				return element.value === recordForEdit['projectStatus'];
+			});
+			const newData = {
+				name: recordForEdit['name'],
+				description: recordForEdit['description'],
+				status: status.value,
+				ipfsFileCID: recordForEdit['ipfsFileCID'],
+			};
+			setValues({
+				...newData,
+			});
+		}
+	}, [recordForEdit]);
+
+	return (
+		<>
+			<Grid container>
+				<Grid item xs={6}>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Name: {values.name}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Description: {values.description}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Status: {values.status}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Pinata File CID: {values.ipfsFileCID}</FormLabel>
+					</p>
+				</Grid>
+			</Grid>
+		</>
+	);
+}
