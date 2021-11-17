b227994d272feda0b0be96a22f6e6e460ae4134a
JBeni <JBeni@users.noreply.github.com>
2021-11-17 15:13:03 +0200

    create view user form

---
+++ src/components/Users/ViewForm.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, FormLabel } from '@material-ui/core';
+import { roleDropdownOptions, initialUserFormValues } from './../applicationService';
+
+export default function ViewForm(props) {
+	const { recordForEdit } = props;
+	const [values, setValues] = useState(initialUserFormValues);
+
+	useEffect(() => {
+		if (recordForEdit != null) {
+			let role = roleDropdownOptions.find((element) => {
+				return element.value === recordForEdit['role'];
+			});
+			let newData = {
+				username: recordForEdit['username'],
+				firstname: recordForEdit['firstname'],
+				lastname: recordForEdit['lastname'],
+				email: recordForEdit['email'],
+				role: role.value,
+				walletAddress: recordForEdit['walletAddress'],
+			};
+			setValues({
+				...newData,
+			});
+		}
+	}, [recordForEdit]);
+
+	return (
+		<>
+			<Grid container>
+				<Grid item xs={6}>
+					<p style={{ width: '650px' }}>
+						<FormLabel>User Name: {values.username}</FormLabel>
+					</p>
+					<p style={{ width: '650px' }}>
+						<FormLabel>First Name: {values.firstname}</FormLabel>
+					</p>
+					<p style={{ width: '650px' }}>
+						<FormLabel>Last Name: {values.lastname}</FormLabel>
+					</p>
+					<p style={{ width: '650px' }}>
+						<FormLabel>Email: {values.email}</FormLabel>
+					</p>
+					<p style={{ width: '650px' }}>
+						<FormLabel>Role: {values.role}</FormLabel>
+					</p>
+					<p style={{ width: '650px' }}>
+						<FormLabel>Wallet Address: {values.walletAddress}</FormLabel>
+					</p>
+				</Grid>
+			</Grid>
+		</>
+	);
+}
