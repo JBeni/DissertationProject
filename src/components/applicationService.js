24cde9309de852b8bc751e35f931d2296564559d
JBeni <JBeni@users.noreply.github.com>
2021-11-17 15:07:15 +0200

    put in service shared resources for user

--- src/components/applicationService.js
+++ src/components/applicationService.js
@@ -1,5 +1,28 @@
+import { projectStatusDropdown } from './Projects/DefaultModal';
 
-import { roleDropdownOptions } from './Users/AddUserModal';
+export const initialUserFormValues = {
+	isEditForm: false,
+	firstname: '',
+	lastname: '',
+	email: '',
+	role: '',
+	walletAddress: '',
+};
+
+export const initialUserFormValidity = {
+	firstname: false,
+	lastname: false,
+	email: false,
+	role: false,
+	walletAddress: false,
+};
+
+export const roleDropdownOptions = [
+	{ id: "1", value: 'DefaultRole' },
+	{ id: "2", value: 'ProjectInitiator' },
+	{ id: "3", value: 'CompanyBuilder' },
+	{ id: "4", value: 'ProjectSupervisor' },
+];
 
 export async function getAllUsers(props) {
     let data = [];
@@ -17,6 +40,30 @@ export async function getAllUsers(props) {
                 walletAddress: result['walletAddress'],
             };
             data.push(user);
+            return false;
+        });
+    }).catch(function (error) {
+        console.log(error);
+    });
+    return data;
+}
+
+export async function getAllProjects(props) {
+    let data = [];
+
+    await this.props.project.methods.getAllProjects().call().then((result) => {
+        result.map((result) => {
+            let status = projectStatusDropdown.find((element) => {
+                return Number(element.id) === Number(result['projectStatus']);
+            });
+            let project = {
+                index: result['index'],
+                name: result['name'],
+                description: result['description'],
+                projectStatus: status.value,
+                ipfsFileCID: result['ipfsFileCID']
+            };
+            this.setState({ projects: [...this.state.projects, project] });
             return false;
         });
     }).catch(function (error) {
