d1f213cbda0d2e4bb0423ee5741002f8e28ccbd2
JBeni <JBeni@users.noreply.github.com>
2021-11-20 22:26:19 +0200

    sidebar role constraints

--- src/components/Navbar/Sidebar.js
+++ src/components/Navbar/Sidebar.js
@@ -26,64 +26,76 @@ function Sidebar(props) {
                                     <span className="nav__name">Dashboard</span>
                                 </Link>
                         }
-
-						<Link
-							to="/users"
-							className={
-								props.currentLocation[1] === 'users'
-									? 'nav__link active'
-									: 'nav__link'
-							}
-						>
-							<i className="bx bx-grid-alt nav__icon"></i>
-							<span className="nav__name">Users</span>
-						</Link>
-						<Link
-							to="/projects"
-							className={
-								props.currentLocation[1] === 'projects'
-									? 'nav__link active'
-									: 'nav__link'
-							}
-						>
-							<i className="bx bx-user nav__icon"></i>
-							<span className="nav__name">Projects</span>
-						</Link>
-						<Link
-							to="/project-initiator"
-							className={
-								props.currentLocation[1] === 'project-initiator'
-									? 'nav__link active'
-									: 'nav__link'
-							}
-						>
-							<i className="bx bx-user nav__icon"></i>
-							<span className="nav__name">Project Initiator</span>
-						</Link>
-
-						<Link
-							to="/company"
-							className={
-								props.currentLocation[1] === 'company'
-									? 'nav__link active'
-									: 'nav__link'
-							}
-						>
-							<i className="bx bx-message-square-detail nav__icon"></i>
-							<span className="nav__name">Company Builder</span>
-						</Link>
-
-						<Link
-							to="/supervisor"
-							className={
-								props.currentLocation[1] === 'supervisor'
-									? 'nav__link active'
-									: 'nav__link'
-							}
-						>
-							<i className="bx bx-bookmark nav__icon"></i>
-							<span className="nav__name">Project Supervisor</span>
-						</Link>
+                        {
+                            props.userRole &&
+                                <Link
+                                    to="/users"
+                                    className={
+                                        props.currentLocation[1] === 'users'
+                                            ? 'nav__link active'
+                                            : 'nav__link'
+                                    }
+                                >
+                                    <i className="bx bx-grid-alt nav__icon"></i>
+                                    <span className="nav__name">Users</span>
+                                </Link>
+                        }
+                        {
+                            props.userRole &&
+                                <Link
+                                    to="/projects"
+                                    className={
+                                        props.currentLocation[1] === 'projects'
+                                            ? 'nav__link active'
+                                            : 'nav__link'
+                                    }
+                                >
+                                    <i className="bx bx-user nav__icon"></i>
+                                    <span className="nav__name">Projects</span>
+                                </Link>
+                        }
+                        {
+                            props.userRole &&
+                                <Link
+                                    to="/project-initiator"
+                                    className={
+                                        props.currentLocation[1] === 'project-initiator'
+                                            ? 'nav__link active'
+                                            : 'nav__link'
+                                    }
+                                >
+                                    <i className="bx bx-user nav__icon"></i>
+                                    <span className="nav__name">Project Initiator</span>
+                                </Link>
+                        }
+                        {
+                            props.userRole &&
+                                <Link
+                                    to="/company"
+                                    className={
+                                        props.currentLocation[1] === 'company'
+                                            ? 'nav__link active'
+                                            : 'nav__link'
+                                    }
+                                >
+                                    <i className="bx bx-message-square-detail nav__icon"></i>
+                                    <span className="nav__name">Company Builder</span>
+                                </Link>
+                        }
+                        {
+                            props.userRole &&
+                                <Link
+                                    to="/supervisor"
+                                    className={
+                                        props.currentLocation[1] === 'supervisor'
+                                            ? 'nav__link active'
+                                            : 'nav__link'
+                                    }
+                                >
+                                    <i className="bx bx-bookmark nav__icon"></i>
+                                    <span className="nav__name">Project Supervisor</span>
+                                </Link>
+                        }
 					</div>
 				</div>
 
