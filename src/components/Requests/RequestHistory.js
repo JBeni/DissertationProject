f5f05e5aaa8fe46a5b557a5b1602ca042559d086
JBeni <JBeni@users.noreply.github.com>
2021-11-23 22:12:36 +0200

    history for requests

---
+++ src/components/Requests/RequestHistory.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import '../Styles/TableHistory.css';
+
+export default function RequestHistory(props) {
+	const { userHistory } = props;
+
+    useEffect(() => {
+        console.log(userHistory);
+    }, [userHistory])
+
+    return (
+		<div className="container-body">
+			<table>
+				<thead>
+					<tr>
+						<th>User Name</th>
+						<th>Email</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Role</th>
+						<th>Wallet Address</th>
+						<th>Timestamp</th>
+					</tr>
+				</thead>
+				<tbody>
+                    {
+                        userHistory.map((item) => (
+                            <tr key={ item.index }>
+                                <td>{ item.username }</td>
+                                <td>{ item.email }</td>
+                                <td>{ item.firstname }</td>
+                                <td>{ item.lastname }</td>
+                                <td>{ item.role }</td>
+                                <td>{ item.walletAddress }</td>
+                                <td>{ item.timestamp }</td>
+                            </tr>
+                        ))
+                    }
+				</tbody>
+			</table>
+		</div>
+	);
+};
