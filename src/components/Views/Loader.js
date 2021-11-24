a0d7d9a70363982ecdc636ac5725a871ddd1fcd8
JBeni <JBeni@users.noreply.github.com>
2021-11-24 19:04:53 +0200

    add text for new use case

--- src/components/Views/Loader.js
+++ src/components/Views/Loader.js
@@ -3,17 +3,29 @@ import { useStylesLoader } from '../sharedResources';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import '../Styles/Loader.css';
 
-export default function Loader() {
+export default function Loader(props) {
     const classes = useStylesLoader();
+    const { isLoading, isUserInBlockchain } = props;
 
 	return (
 		<div className={classes.root}>
 			<CircularProgress size={0.8 * 500} color="primary" className="center-container" />
-            <div className="text-loader">
-                <p>The Project contract not deployed to detected network.</p>
-                <p>You need to start the Ganache deployment network to run this project.</p>
-                <p>You must login into MetaMask then select the Ganache network.</p>
-            </div>
+            {
+                isLoading === true &&
+                <div className="text-loader">
+                    <p>The Project contract not deployed to detected network.</p>
+                    <p>You need to start the Ganache deployment network to run this project.</p>
+                    <p>You must login into MetaMask then select the Ganache network.</p>
+                </div>
+            }
+            {
+                isUserInBlockchain === true &&
+                <div className="text-loader">
+                    <p></p>
+                    <p>Unauthorized Access</p>
+                    <p></p>
+                </div>
+            }
 		</div>
 	);
 }
