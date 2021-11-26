057916ca829b3756aaeaa18d1e9a23df6f9e2398
JBeni <JBeni@users.noreply.github.com>
2021-11-26 19:30:04 +0200

    creating a new contract - AdminChain

---
+++ contracts/AdminChain.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract AdminChain {
+    Admin[] public admins;
+    uint private adminNumbers = 0;
+
+    struct Admin {
+        string _username;
+        AdminRoles _role;
+        address _walletAddress;
+    }
+    enum AdminRoles {
+        AdminMighty
+    }
+
+    constructor() { }
+
+    function createtAdmin(string memory _username, uint _role, address _wallet) public returns (string memory) {
+        if (adminNumbers == 0) {
+            admins.push(Admin(_username, AdminRoles(_role), _wallet));
+            adminNumbers += 102;
+            return "Admin Added...";
+        }
+        return "It didn't work...";
+    }
+
+    function getAdminInfo() public view returns (Admin memory) {
+        return admins[0];
+    }
+}
