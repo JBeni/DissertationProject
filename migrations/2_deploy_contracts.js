6341bd6349d3b0c8bab016ebefd15f20b2f70202
JBeni <JBeni@users.noreply.github.com>
2021-11-26 19:31:40 +0200

    seed data when deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,16 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 var ProjectChain = artifacts.require('ProjectChain');
+var AdminChain = artifacts.require('AdminChain');
 
-module.exports = function (deployer) {
-	deployer.deploy(ProjectChain);
+module.exports = async function (deployer, network, accounts) {
+    await deployer.deploy(ProjectChain);
+
+    const username = "Beniamin Jitca";
+    const role = 0;
+    const walletAddress = accounts[0];
+
+    await deployer.deploy(AdminChain);
+    const result = await AdminChain.deployed().then(function(instance) {
+        return instance.createtAdmin(username, role, walletAddress);
+    });
 };
