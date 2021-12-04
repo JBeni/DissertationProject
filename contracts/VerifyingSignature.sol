6b367eb354d7781bae188b4e1a017f1ea217d9c4
JBeni <JBeni@users.noreply.github.com>
2021-12-04 11:32:24 +0200

    create verify signature contract

--- contracts/VerifyingSignature.sol
+++ contracts/VerifyingSignature.sol
@@ -3,69 +3,14 @@ pragma solidity ^0.8.3 < 0.9.0;
 
 contract VerifySignature {
 
-    function getMessageHash(
-        address _projectAddress
-    ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_projectAddress));
+    // verify signature verifica daca mesajul semnat cu cheia privata apartinei contului cu adresa publica care trebuia sa semneze tranzactia
+    // the buyer address din examplul ala e adresa walletul;ui care a initiat tranzactia
+    function verifySignature(address p, bytes32 hash, uint8 v, bytes32 r, bytes32 s) external pure returns(bool) {
+        return ecrecover(hash, v, r, s) == p;
     }
 
-    function getEthSignedMessageHash(bytes32 _messageHash)
-        public
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
+    function test(bytes32 hash, uint8 v, bytes32 r, bytes32 s) external pure returns(address) {
+        return ecrecover(hash, v, r, s);
     }
 
-    function verify(
-        address _projectAddress,
-        string memory signature
-    ) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(_projectAddress);
-        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
-
-        return recoverSigner(ethSignedMessageHash, signature) == _projectAddress;
-    }
-
-    function recoverSigner(bytes32 _ethSignedMessageHash, string memory _signature)
-        public
-        pure
-        returns (address)
-    {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
-    }
-
-    function splitSignature(string memory sig)
-        public
-        pure
-        returns (
-            bytes32 r,
-            bytes32 s,
-            uint8 v
-        )
-    {
-        require(bytes(sig).length == 65, "invalid signature length");
-
-        assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
-
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        // implicitly return (r, s, v)
-    }
 }
