6b9e036f3753af14f2c945310f679859515ccfd8
JBeni <JBeni@users.noreply.github.com>
2021-12-09 16:28:35 +0200

    implement request history

--- src/components/Requests/RequestHistory.js
+++ src/components/Requests/RequestHistory.js
@@ -1,37 +1,64 @@
 import React, { useEffect } from 'react';
 import '../Styles/TableHistory.css';
+import { Button } from '@material-ui/core';
 
 export default function RequestHistory(props) {
-	const { userHistory } = props;
+	const { requestHistory } = props;
 
     useEffect(() => {
-    }, [userHistory])
+    }, [])
+
+    const verifySignature = async (_projectAddress, _signerAddress, _signature) => {
+        const v = '0x' + _signature.slice(130, 132).toString();
+        const r = _signature.slice(0, 66).toString();
+        const s = '0x' + _signature.slice(66, 130).toString();
+        const messageHash = props.web3.eth.accounts.hashMessage(_projectAddress);
+
+        const signer = await props.web3.eth.accounts.recover(messageHash, v, r, s, true);
+        const verificationOutput = _signerAddress === signer ? true : false;
+
+        if (verificationOutput) {
+            alert('Signer Address is verified successfully!');
+        } else {
+            alert('Signer Address is not verified!');
+        }
+    }
 
     return (
 		<div className="container-body">
 			<table>
 				<thead>
 					<tr>
-						<th>User Name</th>
-						<th>Email</th>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Role</th>
-						<th>Wallet Address</th>
+						<th>Title</th>
+						<th>Project Status</th>
+						<th>Request Status</th>
+						<th>Request Type</th>
+						<th>Project Address</th>
+						<th>Request Address</th>
+						<th>Signer Address</th>
 						<th>Timestamp</th>
+						<th>Signature</th>
+                        <th>Actions</th>
 					</tr>
 				</thead>
 				<tbody>
                     {
-                        userHistory.map((item) => (
+                        requestHistory.map((item) => (
                             <tr key={ item.index }>
-                                <td>{ item.username }</td>
-                                <td>{ item.email }</td>
-                                <td>{ item.firstname }</td>
-                                <td>{ item.lastname }</td>
-                                <td>{ item.role }</td>
-                                <td>{ item.walletAddress }</td>
+                                <td>{ item.title }</td>
+                                <td>{ item.projectStatus }</td>
+                                <td>{ item.requestStatus }</td>
+                                <td>{ item.requestType }</td>
+                                <td>{ item.projectAddress }</td>
+                                <td>{ item.requestAddress }</td>
+                                <td>{ item.signerAddress }</td>
                                 <td>{ item.timestamp }</td>
+                                <td>{ item.signature }</td>
+                                <td>
+                                    <Button variant="contained" color="secondary" onClick={ () =>
+                                        verifySignature(item.projectAddress, item.signerAddress, item.signature)
+                                    }>Verify Signature</Button>
+                                </td>
                             </tr>
                         ))
                     }
