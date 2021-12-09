ac0fcf5dc4afa8d512afc3cba8b055034495ec20
JBeni <JBeni@users.noreply.github.com>
2021-12-09 16:28:12 +0200

    update view request - request folder

--- src/components/Requests/ViewRequest.js
+++ src/components/Requests/ViewRequest.js
@@ -1,30 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, FormLabel } from '@material-ui/core';
+import { Grid, FormLabel, Button } from '@material-ui/core';
 import * as formService from '../Services/formService';
-import * as dropdownService from '../Services/dropdownService';
 
 export default function ViewRequest(props) {
-	const { recordForEdit } = props;
+	const { recordForView } = props;
 	const [values, setValues] = useState(formService.initialRequestFormValues);
 
 	useEffect(() => {
-		if (recordForEdit != null) {
-            const projectStatus = dropdownService.getProjectStatusByValue(recordForEdit.projectStatus);
-            const requestStatus = dropdownService.getRequestStatusByValue(recordForEdit.requestStatus);
-			const newData = {
-				title: recordForEdit.title,
-				projectStatus: projectStatus.value,
-                requestStatus: requestStatus.value,
-                projectAddress: recordForEdit.projectAddress,
-                signerAddress: recordForEdit.signerAddress
-			};
-			setValues({
-				...newData,
-			});
+		if (recordForView != null) {
+            setValues(prev => ({
+				...recordForView,
+                timestamp: new Date(values.timestamp * 1000).toString()
+			}));
 		}
-	}, [recordForEdit]);
+	}, [recordForView]);
 
-	return (
+    return (
 		<>
 			<Grid container>
 				<Grid item xs={6}>
@@ -38,10 +29,19 @@ export default function ViewRequest(props) {
 						<FormLabel>Project Status: {values.projectStatus}</FormLabel>
 					</p>
 					<p style={{ width: '670px' }}>
+						<FormLabel>Request Type: {values.requestType}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
 						<FormLabel>Project Address: {values.projectAddress}</FormLabel>
 					</p>
 					<p style={{ width: '670px' }}>
-						<FormLabel>User Status: {values.signerAddress}</FormLabel>
+						<FormLabel>Request Address: {values.requestAddress}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Signer Address: {values.signerAddress}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Timestamp: {values.timestamp}</FormLabel>
 					</p>
 				</Grid>
 			</Grid>
