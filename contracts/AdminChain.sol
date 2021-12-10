e399e18e18052c38cd72f663d2bcab1957d04157
JBeni <JBeni@users.noreply.github.com>
2021-12-10 08:46:21 +0200

    constraints in admin chain

--- contracts/AdminChain.sol
+++ contracts/AdminChain.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract AdminChain {
-    Admin[] public admins;
+    Admin[] private admins;
     uint private adminNumbers = 0;
 
     struct Admin {
@@ -10,6 +10,7 @@ contract AdminChain {
         AdminRoles _role;
         address _walletAddress;
     }
+
     enum AdminRoles {
         AdminMighty
     }
@@ -17,10 +18,11 @@ contract AdminChain {
     constructor() {}
 
     function createtAdmin(string memory _username, uint _role, address _wallet) public returns (string memory) {
+        require(adminNumbers == 0, "Access Denied!...");
         if (adminNumbers == 0) {
             admins.push(Admin(_username, AdminRoles(_role), _wallet));
             adminNumbers += 102;
-            return "Admin Added...";
+            return "Created...";
         }
         return "It didn't work...";
     }
