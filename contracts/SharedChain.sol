f532ea0b938392f6b803f7cbc3f70e5349d7ad87
JBeni <JBeni@users.noreply.github.com>
2021-12-11 13:44:46 +0200

    reorganize shared chain

--- contracts/SharedChain.sol
+++ contracts/SharedChain.sol
@@ -15,7 +15,6 @@ contract SharedChain {
     }
 
 
-    /******  User Data */
     struct User {
         uint _index;
         string _username;
@@ -32,8 +31,6 @@ contract SharedChain {
         address indexed _walletAddress
     );
 
-
-    /*********  Project Related Data */
     struct Project {
         uint _index;
         string _name;
@@ -49,39 +46,23 @@ contract SharedChain {
         address indexed _projectAddress
     );
 
-    struct ProjectRequest {
+
+    struct Request {
         uint _index;
         string _title;
         string _comments;
-        ProjectStatus _status;
-        RequestStatus _requestStatus;
-        address _projectAddress;
-        address _signerAddress;
-        address _requestAddress;
-        uint _timestamp;
-        string _signature;
-    }
-    event ProjectRequestEvent(
-        ProjectRequest _projectRequest,
-        address indexed _projectAddress
-    );
-
-    /******** Supervisor and Company Data */
-    struct Request {
-        uint _index;
-        uint _indexProjectRequest;
-        string _title;
-        RequestStatus _requestStatus;
         ProjectStatus _projectStatus;
+        RequestStatus _requestStatus;
         RequestType _requestType;
         address _projectAddress;
-        address _signerAddress;
         address _requestAddress;
+        address _signerAddress;
         uint _timestamp;
         string _signature;
     }
     event RequestEvent(
         Request _request,
-        address indexed _requestAddress
+        address indexed _requestAddress,
+        address indexed _projectAddress
     );
 }
