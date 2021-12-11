468bf38321154b3e50d0f164a14ae5aa355740e1
JBeni <JBeni@users.noreply.github.com>
2021-12-11 13:45:27 +0200

    role guard on project add form

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,7 @@ import { withRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import * as eventService from '../Services/eventService';
 import * as toasterService from '../Services/toasterService';
+import * as roleService from '../Services/roleService';
 import HistoryIcon from '@mui/icons-material/History';
 import Edit from '@material-ui/icons/Edit';
 import ProjectHistory from './ProjectHistory';
@@ -206,22 +207,25 @@ class Projects extends Component {
 						this.setRecordForEdit(null);
 					}}>Add New</Button>
 
-				<Dialog open={this.state.openAddForm} maxWidth="md">
-					<DialogTitle>
-						<div style={{ display: 'flex' }}>
-							<Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
-								Add Project Form
-							</Typography>
-							<Button color="secondary" onClick={() => {
-									this.setOpenAddForm(false);
-								}}><CloseIcon />
-							</Button>
-						</div>
-					</DialogTitle>
-					<DialogContent dividers style={{ width: '700px' }}>
-                        <AddProjectForm handleNewDataFromPopup={this.handleNewDataFromPopup.bind(this)} recordForEdit={this.state.recordForEdit} addOrEdit={this.addOrEdit} />
-                    </DialogContent>
-				</Dialog>
+                {
+                    this.props.currentUserRole === roleService.getUserProjectRole() &&
+                    <Dialog open={this.state.openAddForm} maxWidth="md">
+                        <DialogTitle>
+                            <div style={{ display: 'flex' }}>
+                                <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+                                    Add Project Form
+                                </Typography>
+                                <Button color="secondary" onClick={() => {
+                                        this.setOpenAddForm(false);
+                                    }}><CloseIcon />
+                                </Button>
+                            </div>
+                        </DialogTitle>
+                        <DialogContent dividers style={{ width: '700px' }}>
+                            <AddProjectForm handleNewDataFromPopup={this.handleNewDataFromPopup.bind(this)} recordForEdit={this.state.recordForEdit} addOrEdit={this.addOrEdit} />
+                        </DialogContent>
+                    </Dialog>
+                }
 
 				<Dialog open={this.state.openViewForm} maxWidth="md">
 					<DialogTitle>
