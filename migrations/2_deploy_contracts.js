03c12d097905e404a545f8c7f113cc45837d951c
JBeni <JBeni@users.noreply.github.com>
2021-12-11 20:30:35 +0200

    update deploy contract script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,13 +9,14 @@ module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(ProjectChain);
     await deployer.deploy(UserChain);
 
-    const username = "Beniamin Jitca";
+    const firstname = "Jitca";
+    const lastname = "Jitca";
     const role = 0;
     const walletAddress = accounts[0];
 
     await deployer.deploy(AdminChain);
     await AdminChain.deployed().then(async function(instance) {
-        return await instance.createtAdmin(username, role, walletAddress);
+        return await instance.createtAdmin(firstname, lastname, role, walletAddress);
     });
 
     await deployer.deploy(ServiceChain);
