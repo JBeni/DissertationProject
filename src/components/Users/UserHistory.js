c189bdd8392b740241a593dcea6de4242db17e4b
JBeni <JBeni@users.noreply.github.com>
2021-12-11 20:34:42 +0200

    user history comp

--- src/components/Users/UserHistory.js
+++ src/components/Users/UserHistory.js
@@ -20,28 +20,22 @@ export default function UserHistory(props) {
 			<table>
 				<thead>
 					<tr>
-						<th>User Name</th>
-						<th>Email</th>
 						<th>First Name</th>
 						<th>Last Name</th>
 						<th>Role</th>
 						<th>Wallet Address</th>
 						<th>Timestamp</th>
-						<th>Signature</th>
 					</tr>
 				</thead>
 				<tbody>
                     {
                         userHistory.map((item) => (
                             <tr key={ item.index }>
-                                <td>{ item.username }</td>
-                                <td>{ item.email }</td>
                                 <td>{ item.firstname }</td>
                                 <td>{ item.lastname }</td>
                                 <td>{ item.role }</td>
                                 <td>{ item.walletAddress }</td>
                                 <td>{ item.timestamp }</td>
-                                <td>{ item.signature }</td>
                             </tr>
                         ))
                     }
