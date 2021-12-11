e6ba533b5baabd4e90fb67263dde759c6a158a41
JBeni <JBeni@users.noreply.github.com>
2021-12-11 19:58:07 +0200

    update user contract - remove stuff

--- contracts/UserChain.sol
+++ contracts/UserChain.sol
@@ -8,57 +8,39 @@ contract UserChain is SharedChain {
 
     mapping(address => User) public users;
     address[] usersAddress;
-    uint256 usersCounter = 0;
 
     constructor() {
         owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        //require(owner == msg.sender, "You are not an owner.");
+        require(owner == msg.sender, "You are not an owner.");
         _;
     }
 
     modifier checkUser(address _address) {
-        //require(_address == owner, "You are not an owner.");
+        require(_address == owner, "You are not an owner.");
         _;
     }
 
     function registerUser(
-        string memory _username,
-        bytes32 _email,
         bytes32 _firstname,
         bytes32 _lastname,
         uint256 _role,
-        address _walletAddress,
-        string memory _signature
+        address _walletAddress
     ) public onlyOwner {
         users[_walletAddress] = User(
-            usersCounter, _username, _email, _firstname, _lastname,
-            Roles(_role), _walletAddress, block.timestamp, _signature
+            _firstname, _lastname,
+            Roles(_role), _walletAddress, block.timestamp
         );
 
         emit UserEvent(users[_walletAddress], _walletAddress);
-        usersAddress.push(_walletAddress);
-        usersCounter++;
     }
 
-    function changeUserRole(uint256 _role, address _walletAddress, string memory _signature) public onlyOwner {
+    function changeUserRole(uint256 _role, address _walletAddress) public onlyOwner {
         users[_walletAddress]._role = Roles(_role);
-
         users[_walletAddress]._timestamp = block.timestamp;
-        users[_walletAddress]._signature = _signature;
         emit UserEvent(users[_walletAddress], _walletAddress);
-    }
-
-    function getAllUsers() public view onlyOwner returns(User[] memory) {
-        User[] memory allUsers = new User[](usersCounter);
-        for (uint256 index = 0; index < usersCounter; index++) {
-            address _walletAddress = usersAddress[index];
-            User storage user = users[_walletAddress];
-            allUsers[index] = user;
-        }
-        return allUsers;
     }
 
     function getUserInfo(address _walletAddress) public view returns (User memory) {
