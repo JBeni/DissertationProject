99f663eb2c4faea4436e8ca547bb93207d0daffe
JBeni <JBeni@users.noreply.github.com>
2021-12-12 13:51:27 +0200

    create view company comp

---
+++ src/components/Company/ViewCompanyRequest.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, FormLabel } from '@material-ui/core';
+import * as formService from '../Services/formService';
+import * as dropdownService from '../Services/dropdownService';
+
+export default function ViewCompanyRequest(props) {
+	const { recordForEdit } = props;
+	const [values, setValues] = useState(formService.initialProjectFormValues);
+
+	useEffect(() => {
+		if (recordForEdit != null) {
+            const projectStatus = dropdownService.getProjectStatusByValue(recordForEdit.projectStatus);
+            const requestStatus = dropdownService.getRequestStatusByValue(recordForEdit.requestStatus);
+            setValues(prev => ({
+				...recordForEdit,
+				projectStatus: projectStatus.value,
+                requestStatus: requestStatus.value
+			}));
+		}
+	}, [recordForEdit]);
+
+	return (
+		<>
+			<Grid container>
+				<Grid item xs={6}>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Title: {values.title}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Request Status: {values.requestStatus}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Project Status: {values.projectStatus}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Project Address: {values.projectAddress}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>Signer Address: {values.signerAddress}</FormLabel>
+					</p>
+				</Grid>
+			</Grid>
+		</>
+	);
+}
