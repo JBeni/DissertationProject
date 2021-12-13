f3cbe5f5e4cfddb095dcb33049eb68b228813750
JBeni <JBeni@users.noreply.github.com>
2021-12-13 13:04:37 +0200

    update view request

--- src/components/Requests/ViewRequest.js
+++ src/components/Requests/ViewRequest.js
@@ -13,7 +13,23 @@ export default function ViewRequest(props) {
                 timestamp: new Date(values.timestamp * 1000).toString()
 			}));
 		}
-	}, [recordForView]);
+	}, []);
+
+    const verifySignature = async (_requestAddress, _signerAddress, _signature) => {
+        const v = '0x' + _signature.slice(130, 132).toString();
+        const r = _signature.slice(0, 66).toString();
+        const s = '0x' + _signature.slice(66, 130).toString();
+        const messageHash = props.web3.eth.accounts.hashMessage(_requestAddress);
+
+        const signer = await props.web3.eth.accounts.recover(messageHash, v, r, s, true);
+        const verificationOutput = _signerAddress === signer ? true : false;
+
+        if (verificationOutput) {
+            alert('Signer Address is verified successfully!');
+        } else {
+            alert('Signer Address is not verified!');
+        }
+    }
 
     return (
 		<>
@@ -40,8 +56,18 @@ export default function ViewRequest(props) {
 					<p style={{ width: '670px' }}>
 						<FormLabel>Signer Address: {values.signerAddress}</FormLabel>
 					</p>
-					<p style={{ width: '670px' }}>
+					<p style={{ width: '670px', wordBreak: 'break-word' }}>
 						<FormLabel>Timestamp: {values.timestamp}</FormLabel>
+					</p>
+					<p style={{ width: '670px', wordBreak: 'break-word' }}>
+						<FormLabel>Signature: {values.signature}</FormLabel>
+					</p>
+					<p style={{ width: '670px' }}>
+						<FormLabel>
+                            <Button variant="contained" color="secondary" onClick={ () =>
+                                verifySignature(values.requestAddress, values.signerAddress, values.signature)
+                            }>Verify Signature</Button>
+                        </FormLabel>
 					</p>
 				</Grid>
 			</Grid>
