5371898783ef2085a5c4956435da4b42424f4642
JBeni <JBeni@users.noreply.github.com>
2021-12-15 09:51:47 +0200

    require constraints on Admin contract

--- contracts/AdminChain.sol
+++ contracts/AdminChain.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 contract AdminChain {
     Admin[] private admins;
     uint256 private adminNumbers = 0;
+    address public owner = msg.sender;
 
     struct Admin {
         string _firstname;
@@ -19,12 +20,13 @@ contract AdminChain {
     constructor() {}
 
     modifier onlyAdmin() {
-        //require(address(0xf08B741073b3Cb01ef6fB3B412E71C977F276fAa) == msg.sender, "You are not the right user.");
+        require(address(0x0) != msg.sender, "Address is not valid.");
+        require(owner == msg.sender, "You are not the right user.");
         _;
     }
 
     function createtAdmin(string memory _firstname, string memory _lastname, uint256 _role, address _wallet) public onlyAdmin returns (string memory) {
-        //require(adminNumbers == 0, "Access Denied!...");
+        require(adminNumbers == 0, "Access Denied!...");
         if (adminNumbers == 0) {
             admins.push(Admin(_firstname, _lastname, AdminRoles(_role), _wallet));
             adminNumbers += 102;
