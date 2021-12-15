03da582a1a4b825ededb26dce0e116c37558290c
JBeni <JBeni@users.noreply.github.com>
2021-12-15 10:44:19 +0200

    remove methods event service

--- src/components/Services/eventService.js
+++ src/components/Services/eventService.js
@@ -1,30 +1,7 @@
 import * as dropdownService from './dropdownService';
 
-/** User Events Methods */
 
-export async function getAllUserEvents(props) {
-    const events = await props.userChain.getPastEvents('UserEvent', {
-        fromBlock: 0,
-        toBlock: 'latest'
-    });
-    if (events === undefined || events.length === 0) return [];
-
-    let dataArray = [];
-    events.map((result) => {
-        const role = dropdownService.getUserRoleById(result.returnValues._user._role);
-        const user = {
-            index: Number(result.returnValues._user._index),
-            firstname: props.web3.utils.hexToUtf8(result.returnValues._user._firstname),
-            lastname: props.web3.utils.hexToUtf8(result.returnValues._user._lastname),
-            role: role.value,
-            walletAddress: result.returnValues._user._walletAddress,
-            timestamp: new Date(result.returnValues._user._timestamp * 1000).toString(),
-        };
-        dataArray.push(user);
-        return false;
-    });
-    return dataArray;
-}
+/*****************/
 
 export async function getUserEvents(props, _walletAddress) {
     const events = await props.userChain.getPastEvents('UserEvent', {
@@ -48,35 +25,6 @@ export async function getUserEvents(props, _walletAddress) {
         };
         dataArray.push(user);
         indexLocal++;
-        return false;
-    });
-    return dataArray;
-}
-
-
-/** Project Events Methods */
-
-export async function getAllProjectEvents(props) {
-    const events = await props.project.getPastEvents('ProjectEvent', {
-        fromBlock: 0,
-        toBlock: 'latest'
-    });
-    if (events === undefined || events.length === 0) return [];
-
-    let dataArray = [];
-    events.map((result) => {
-        const status = dropdownService.getProjectStatusById(result.returnValues._project._status);
-        const project = {
-            index: Number(result.returnValues._project._index),
-            name: result.returnValues._project._name,
-            status: status.value,
-            ipfsFileCID: result.returnValues._project._ipfsFileCID,
-            projectAddress: result.returnValues._project._projectAddress,
-            signerAddress: result.returnValues._project._signerAddress,
-            timestamp: new Date(result.returnValues._project._timestamp * 1000).toString(),
-            signature: result.returnValues._project._signature
-        };
-        dataArray.push(project);
         return false;
     });
     return dataArray;
@@ -156,72 +104,6 @@ export async function getProjectRequestEvents(props, _projectAddress) {
     });
     return dataArray;
 }
-
-
-/*** REQUEST METHODS */
-
-export async function getRequestEvents(props, _requestAddress) {
-    const events = await props.project.getPastEvents('RequestEvent', {
-        filter: { _requestAddress: _requestAddress },
-        fromBlock: 0,
-        toBlock: 'latest'
-    });
-    if (events === undefined || events.length === 0) return [];
-
-    let dataArray = [];
-    events.map((result) => {
-        const projectStatus = dropdownService.getProjectStatusById(result.returnValues._request._projectStatus);
-        const requestStatus = dropdownService.getRequestStatusById(result.returnValues._request._requestStatus);
-        const requestType = dropdownService.getRequestTypeById(result.returnValues._request._requestType);
-        const project = {
-            index: Number(result.returnValues._request._index),
-            title: result.returnValues._request._title,
-            projectStatus: projectStatus.value,
-            requestStatus: requestStatus.value,
-            requestType: requestType.value,
-            projectAddress: result.returnValues._request._projectAddress,
-            requestAddress: result.returnValues._request._requestAddress,
-            signerAddress: result.returnValues._request._signerAddress,
-            timestamp: new Date(result.returnValues._request._timestamp * 1000).toString(),
-            signature: result.returnValues._request._signature
-        };
-        dataArray.push(project);
-        return false;
-    });
-    return dataArray;
-}
-
-export async function getSupervisorRequestEvents(props, _requestAddress) {
-    const events = await props.project.getPastEvents('RequestEvent', {
-        filter: { _requestAddress: _requestAddress },
-        fromBlock: 0,
-        toBlock: 'latest'
-    });
-    if (events === undefined || events.length === 0) return [];
-
-    let dataArray = [];
-    events.map((result) => {
-        const projectStatus = dropdownService.getProjectStatusById(result.returnValues._request._projectStatus);
-        const requestStatus = dropdownService.getRequestStatusById(result.returnValues._request._requestStatus);
-        const requestType = dropdownService.getRequestTypeById(result.returnValues._request._requestType);
-        const project = {
-            index: Number(result.returnValues._request._index),
-            title: result.returnValues._request._title,
-            projectStatus: projectStatus.value,
-            requestStatus: requestStatus.value,
-            requestType: requestType.value,
-            projectAddress: result.returnValues._request._projectAddress,
-            requestAddress: result.returnValues._request._requestAddress,
-            signerAddress: result.returnValues._request._signerAddress,
-            timestamp: new Date(result.returnValues._request._timestamp * 1000).toString(),
-            signature: result.returnValues._request._signature
-        };
-        dataArray.push(project);
-        return false;
-    });
-    return dataArray;
-}
-
 
 export async function getAllRequestEvents(props) {
     const events = await props.project.getPastEvents('RequestEvent', {
