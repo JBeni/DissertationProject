a80b1460c2fe2f2848d662e41ab9fca2c7d2f1e3
JBeni <JBeni@users.noreply.github.com>
2021-12-15 09:52:31 +0200

    require constraints on Shared contract

--- contracts/SignatureChain.sol
+++ contracts/SignatureChain.sol
@@ -5,16 +5,16 @@ contract SignatureChain {
 
     constructor() {}
 
-    modifier onlyOwner() {
-        require(address(0x0) == msg.sender, "The address is not valid.");
+    modifier onlyValidAddress() {
+        require(address(0x0) != msg.sender, "Address is not valid.");
         _;
     }
 
-    function verifySignature(address p, bytes32 hash, uint8 v, bytes32 r, bytes32 s) external view onlyOwner returns(bool) {
+    function verifySignature(address p, bytes32 hash, uint8 v, bytes32 r, bytes32 s) external view onlyValidAddress returns(bool) {
         return ecrecover(hash, v, r, s) == p;
     }
 
-    function verifyTest(bytes32 hash, uint8 v, bytes32 r, bytes32 s) external view onlyOwner returns(address) {
+    function verifyTest(bytes32 hash, uint8 v, bytes32 r, bytes32 s) external view onlyValidAddress returns(address) {
         return ecrecover(hash, v, r, s);
     }
 }
