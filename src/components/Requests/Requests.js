1910f4cb1ef0f15921f54f715559d65f98e99cf0
JBeni <JBeni@users.noreply.github.com>
2021-12-15 10:31:41 +0200

    remove code from requests

--- src/components/Requests/Requests.js
+++ src/components/Requests/Requests.js
@@ -5,19 +5,14 @@ import { Typography, Button, Dialog, DialogTitle, DialogContent } from '@materia
 import CloseIcon from '@material-ui/icons/Close';
 import ViewRequest from './ViewRequest';
 import MaterialTable from '@material-table/core';
-import * as applicationService from '../Services/applicationService';
-import HistoryIcon from '@mui/icons-material/History';
 import * as eventService from '../Services/eventService';
-import RequestHistory from './RequestHistory';
 
 class Requests extends Component {
     constructor(props) {
         super(props);
         this.state = {
             viewRequest: false,
-            openRequestHistory: false,
 			recordForView: null,
-            requestHistory: [],
             requests: [],
         };
     }
@@ -38,18 +33,6 @@ class Requests extends Component {
 	setRecordForView = (data) => {
 		this.setState({ recordForView: data });
 	}
-
-    setOpenRequestHistory = (value) => {
-        this.setState({ openRequestHistory: value });
-        if (value === false) {
-            this.setState({ requestHistory: [] });
-        }
-    }
-
-    setRequestHistory = async (rowData) => {
-        const data = await eventService.getRequestEvents(this.props, rowData.requestAddress);
-        this.setState({ requestHistory: data });
-    }
 
     render() {
         const tableRef = React.createRef();
@@ -79,23 +62,6 @@ class Requests extends Component {
                     </DialogContent>
 				</Dialog>
 
-				<Dialog open={this.state.openRequestHistory} fullWidth maxWidth="xl">
-					<DialogTitle>
-						<div style={{ display: 'flex' }}>
-							<Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
-								View Request Changes
-							</Typography>
-							<Button color="secondary" onClick={() => {
-									this.setOpenRequestHistory(false);
-								}}><CloseIcon />
-							</Button>
-						</div>
-					</DialogTitle>
-					<DialogContent dividers>
-                        <RequestHistory web3={this.props.web3} requestHistory={this.state.requestHistory} />
-                    </DialogContent>
-				</Dialog>
-
                 <br />
 				<MaterialTable
 					title="All Completed Requests"
@@ -109,14 +75,6 @@ class Requests extends Component {
                         pageSize: 10,
                     }}
 					actions={[
-                        // {
-						// 	icon: HistoryIcon,
-						// 	tooltip: 'View Request History',
-						// 	onClick: (event, rowData) => {
-                        //         this.setOpenRequestHistory(true);
-                        //         this.setRequestHistory(rowData);
-						// 	},
-						// },
                         {
 							icon: Visibility,
 							tooltip: 'View Request',
