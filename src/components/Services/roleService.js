f73ece9d81d9d6501a6a3cb0558d87e222b4b3cd
JBeni <JBeni@users.noreply.github.com>
2021-12-17 22:52:43 +0200

    update role service

--- src/components/Services/roleService.js
+++ src/components/Services/roleService.js
@@ -1,24 +1,30 @@
 
 export async function getAddressZeroValue(props) {
-    return await props.serviceChain.methods.getAddressZeroValue().call({ from: props.account });
+    return await props.serviceChain.methods.getAddressZeroValue().call({ from: props.account })
+        .then((response) => { return response; }).catch((error) => {});
 }
 
 export async function getAdminRole(props) {
-    return await props.serviceChain.methods.getAdminRole().call({ from: props.account });
+    return await props.serviceChain.methods.getAdminRole().call({ from: props.account })
+        .then((response) => { return response; }).catch((error) => {});
 }
 
 export async function getDefaultRole(props) {
-    return await props.serviceChain.methods.getDefaultRole().call({ from: props.account });
+    return await props.serviceChain.methods.getDefaultRole().call({ from: props.account })
+        .then((response) => { return response; }).catch((error) => {});
 }
 
 export async function getUserProjectRole(props) {
-    return await props.serviceChain.methods.getUserProjectRole().call({ from: props.account });
+    return await props.serviceChain.methods.getUserProjectRole().call({ from: props.account })
+        .then((response) => { return response; }).catch((error) => {});
 }
 
 export async function getCompanyRole(props) {
-    return await props.serviceChain.methods.getCompanyRole().call({ from: props.account });
+    return await props.serviceChain.methods.getCompanyRole().call({ from: props.account })
+        .then((response) => { return response; }).catch((error) => {});
 }
 
 export async function getSupervisorRole(props) {
-    return await props.serviceChain.methods.getSupervisorRole().call({ from: props.account });
+    return await props.serviceChain.methods.getSupervisorRole().call({ from: props.account })
+        .then((response) => { return response; }).catch((error) => {});
 }
